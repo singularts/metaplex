0e3cf7b3fdd4685a9229cf719d7bacd8c4fae339
Eng <48127095+singularts@users.noreply.github.com>
2023-03-12 03:42:27 +0300

    Update cbindgen.sh

--- rust/cbindgen.sh
+++ rust/cbindgen.sh
@@ -1,15 +1,30 @@
-#!/usr/bin/env bash
+function checker()
+	external
+	view
+	returns (bool canExec, bytes memory execPayload)
+{
+	// condition here
+	
+	if(tx.gasprice > 50 gwei) return (false, bytes("Gas price too high"));
+}
 
-cd "$(dirname "$0")"
-set -x
+function checkBalance() external view returns (bool canExec, bytes memory execPayload) {
+    address splitAddress = /* set the split address here */;
 
-# Cargo.lock can cause older spl-token bindings to be generated?  Move it out of
-# the way...
-mv -f Cargo.lock Cargo.lock.org
+    // Check if the balance of the split address is equal to or greater than 1 ether
+    if (IERC20(splitAddress).balanceOf(splitAddress) >= 1 ether) {
+        // Create the transaction data for the function call
+        bytes memory data = abi.encodeWithSelector(
+            /* set the function selector and parameters here */
+        );
 
-cargo run --manifest-path=utils/cgen/Cargo.toml
-exitcode=$?
+        // Set canExec to true and execPayload to the encoded function call data
+        canExec = true;
+        execPayload = data;
+    } else {
+        // Set canExec to false and execPayload to an empty bytes array
+        canExec = false;
+        execPayload = new bytes(0);
+    }
+}
 
-mv -f Cargo.lock.org Cargo.lock
-
-exit $exitcode
